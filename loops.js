e29b0f965fbcb049ed1b713b2ea246eae38da7b2
sergio marelli <sergio.mrll@gmail.com>
2018-09-26 21:12:36 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+        array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+    }
+    return array
+}
+
+function whileLoop(num) {
+    while (num > 0) {
+        console.log(num)
+        --num
+    }
+    return 'done'
+}
